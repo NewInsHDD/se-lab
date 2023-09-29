4141875dfbf702748012fcb57ba6019f12784a89
szasomaa@gmail.com <szasomaa@gmail.com>
2023-09-29 12:48:03 +0200

    Torpedo ALL feature is done

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+        //FEATURE IMPLEMENTED!!!!
         if (! primaryTorpedoStore.isEmpty()) {
           firingSuccess = primaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = true;
